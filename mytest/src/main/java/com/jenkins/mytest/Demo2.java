e3cf21170d8d4755c1eb6b1f7553c63dd0937d39
xincheng111 <905015101@qq.com>
2019-08-10 19:39:26 +0800

    上传新的代码

---
+++ mytest/src/main/java/com/jenkins/mytest/Demo2.java
@@ -0,0 +1,18 @@
+package com.jenkins.mytest;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	@Test
+	public void test001() {
+		System.out.println("这个是test001");
+	}
+	@Test
+	public void test002() {
+		System.out.println("这个是test002");
+	}
+	@Test
+	public void test003() {
+		System.out.println("这个是test003");
+	}
+}
